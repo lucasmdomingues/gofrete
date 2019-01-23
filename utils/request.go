f2a9d2ee2af6ce92d6362a88d4724042af6b2a3e
Lucas Martins Domingues <lucas.progmd@gmail.com>
2019-01-23 17:49:57 -0200

    commit

--- utils/request.go
+++ utils/request.go
@@ -9,6 +9,7 @@ func MakeRequest(w http.ResponseWriter, route string) ([]byte, error) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 	resp, err := http.Get(route)
 	if err != nil {
