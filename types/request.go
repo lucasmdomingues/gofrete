f362d73b604334bc8f9510ad874ac63fab99ff46
Lucas Martins Domingues <lucas.progmd@gmail.com>
2019-01-24 16:12:14 -0200

    commit

--- types/request.go
+++ types/request.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 )
@@ -17,15 +18,17 @@ func (r *Request) SendRequest() ([]byte, error) {
 	r.ResponseWriter.Header().Set("Access-Control-Allow-Origin", "*")
 	r.ResponseWriter.Header().Add("Access-Control-Allow-Credentials", "true")
 
-	resp, err := http.Get(r.Route)
+	req, err := http.NewRequest(r.Method, r.Route, bytes.NewBuffer(r.Values))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
 
 	return body, nil
 }
